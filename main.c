abefaa90376cc7e454bc9993d33bea299a0f48f8
доминик торето <sergo.matter@yandex.ru>
2024-12-27 02:21:46 +0900

    Use proper problem
    Signed-off-by: доминик торето <sergo.matter@yandex.ru>

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ fgets(first, 255, stdin);
 first[strlen(first)-1] = '\0'; /* remove the newline at the end */
 printf("Now enter your last name: ");
 gets(last); /* buffer overflow? what's that? */
-printf("Hello %s %s!\n", first, last);
+printf("Hello, %s %s!\n", first, last);
 return 0;
 }
 
